446e9114d10be624e10bc9315acaf288a64d01a3
Carl Boettiger <cboettig@gmail.com>
2019-08-13 14:46:05 -0400

    slides and symlinks

---
+++ slides/gh-pages.R
@@ -0,0 +1,11 @@
+library(fs)
+library(here)
+
+fs::dir_create(here("docs"))
+
+slides <- here("slides/solarized.html")
+fs::link_create(slides, here("docs/index.html"))
+fs::link_create(here("slides/img"), here("docs/img"))
+fs::link_create(here("slides/libs"), here("docs/libs"))
+fs::link_create(here("slides/solarized_files"), here("docs/solarized_files"))
+fs::link_create(here("slides/solarized-light.css"), here("docs/solarized-light.css"))
